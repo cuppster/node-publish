773a3373dfa763ee0c01a0cf4fbd46fe956df874
jason <jason@jasuntu.(none)>
2011-05-22 07:58:51 -0700

    handle date paths for posts YYYY-MM-DD and pages in pages/ folder

--- site.js
+++ site.js
@@ -12,6 +12,19 @@ var
   md = require("node-markdown").Markdown;
 
 /**
+ * regex used in validating a url
+ */
+var
+  // starts with alpha then only alpha, digits and underscore and dash are allowed
+  pageNameRegex = /^[a-z][a-z0-9-]+$/;
+  // yyyy/mm/dd
+  datePathRegex = /^\d{4}\/\d{2}\/\d{2}$/;
+  
+var
+  // used with scanning the pages directory for posts
+  postFileRegex = /^(\d{4})-(\d{2})-(\d{2})-([a-z][a-z0-9-]+)\.md/;
+  
+/**
  * index
  */
 Site.index = function(req, res, next)  {  
@@ -40,7 +53,7 @@ Site.index = function(req, res, next)  {
       // collect posts
       var posts = [];
       for (var i in files) {       
-        var match = /(\d{4})-(\d{2})-(\d{2})-([a-z][a-z0-9-]+)/.exec(files[i]);
+        var match = postFileRegex.exec(files[i]);
         if (match) {
           
           // extract the title of the markdown page
@@ -97,49 +110,90 @@ Site.indexStatic = function(req, res, next)  {
 }
 
 /**
- * get post by date and slug
+ * get post by date (YY/MM/DD) and slug
  */
 Site.showPost = function(req, res, next) {  
   req.params['date'] = req.params[0] + '-' + req.params[1] + '-' + req.params[2];
+  req.params['datePath'] = req.params[0] + '/' + req.params[1] + '/' + req.params[2];
   req.params['id'] = req.params[3];   
   Site.show(req, res, next);
 }
 
 /**
- * show
+ * show post or page
  */
 Site.show = function(req, res, next)  {  
   
-  // requested page
-  var pageDate = req.params.date;  
-  var pageName = req.params.id;  
+  // get params
+  var 
+    pageDate = req.params.date,  
+    pageName = req.params.id,
+    pageDatePath = req.params.datePath;
   
-  // validate
-  try {
-    // starts with alpha then only alpha, digits and underscore and dash are allowed
-    check(pageName, "not found").notEmpty().regex(/^[a-z][a-z0-9-]+$/, 'i');
+  // validate page name
+  check(pageName, "bad page").notEmpty().regex(pageNameRegex, 'i');
+  
+  // validate date path
+  if (pageDatePath)
+    check(pageDatePath, "bad date").notEmpty().regex(datePathRegex, 'i');
+  
+  // construct filename from page slug
+  var filename = pageName + '.md';
+  
+  if (pageDate) {
+    /**
+     * post, yyyy-mm-dd-slug.md
+     */
+    filename = pageDate + '-' + filename;
+  }
+  else {
+    /**
+     * page, pages/slug.md
+     */
+    filename = 'pages/' + filename;
+  }
+  
+  /**
+   * check if file exists
+   */
+  var fullPath = path.join(opt.pagesPath, filename);
+  if (!path.existsSync(fullPath)) {
+    fs.readdir(opt.pagesPath, function(err, files) {
+      var re = new RegExp("^(\\d{4})-(\\d{2})-(\\d{2})-" + pageName + "\\.md$");
+      for (var i in files) {
+        var match = re.exec(files[i]);
+        if (match) {
+          var redirUrl = opt.siteUrl + "/" + match[1] + '/' + match[2] + '/' + match[3] + '/' + pageName;
+          res.redirect(redirUrl);
+          return;
+        }         
+      }
+      
+      /**
+       * not found
+       */
+      next(new Error("not found!"));
+      
+    });
+  }
+  else /* OK */ {    
     
+    var permalink = pageDate  ? (opt.siteUrl + '/' + pageDatePath + '/' + pageName)
+                              : (opt.siteUrl + '/' + pageName);
+                              
+    var pageClass = pageDate ? 'post' : 'page';
+    
+    // render      
     var args = {
       isMobile:   req.isMobile,
       isIndex:    false,
       layout:     (req.isMobile ? 'mobile/page.jade' : 'page.jade'),
       page:       {
-                    class: 'page', 
+                    class: pageClass,
                     id: crypto.createHash('md5').update(pageName).digest('hex'),
-                    permalink:  opt.siteUrl + '/' + pageName,
+                    permalink:  permalink,
                   },
-    };
-    
-    // construct filename
-    var filename = pageName + '.md';
-    if ('' != pageDate) {
-      filename = pageDate + '-' + filename;
-    }    
-    
-    // render
+    };    
     res.render(path.join(opt.pagesPath, filename), args);  
-  }
-  catch (err) {
-    next(err);
   }
 };
